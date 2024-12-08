3fd2964b9d523d5100f52b3a42077f95bde37ace
Lover1975 <drjoenh1983@gmail.com>
2024-12-08 08:08:06 -0800

    Category Management APIs.

--- web-backend/src/main/java/com/example/webbackend/web/BaseResponse.java
+++ web-backend/src/main/java/com/example/webbackend/web/BaseResponse.java
@@ -1,25 +1,23 @@
 package com.example.webbackend.web;
 
-import com.example.webbackend.repository.entity.dtos.Dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class BaseResponse {
+public class BaseResponse<T> { // Add a generic type parameter T
     @JsonProperty("responseHeader")
     private ResponseHeader responseHeader;
 
-    @JsonProperty("dto")
-    private Dto dto;
+    @JsonProperty("data") // Rename this field to a more general name like "data"
+    private T data; // Use the generic type T instead of Dto
 
-    public BaseResponse(ResponseHeader responseHeader, Dto dto){
+    public BaseResponse(ResponseHeader responseHeader, T data) {
         this.responseHeader = responseHeader;
-        this.dto = dto;
+        this.data = data;
     }
 
-    public BaseResponse(){
-
+    public BaseResponse() {
     }
 }
